9768b163751958787570df1ef27998069362749e
Andreas Schildbach <andreas@schildbach.de>
2019-10-15 23:12:44 +0200

    TripOptions: Fix a JavaDoc reference.

--- enabler/src/thepublictransport/schildbach/pte/dto/TripOptions.java
+++ enabler/src/thepublictransport/schildbach/pte/dto/TripOptions.java
@@ -17,6 +17,7 @@
 
 package thepublictransport.schildbach.pte.dto;
 
+import java.util.Date;
 import java.util.Set;
 
 import javax.annotation.Nullable;
@@ -31,7 +32,7 @@ import thepublictransport.schildbach.pte.NetworkProvider.TripFlag;
 import thepublictransport.schildbach.pte.NetworkProvider.WalkSpeed;
 
 /**
- * Options for {@link NetworkProvider#queryTrips()}.
+ * Options for {@link NetworkProvider#queryTrips(Location, Location, Location, Date, boolean, TripOptions)}.
  * 
  * @author Ialokim
  */
